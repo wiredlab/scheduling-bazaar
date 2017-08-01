1f912858bfc32f0369ade48b4d9ec1b548152d69
Grace Freigang <grace.freigang@valpo.edu>
2017-08-01 14:35:21 +0000

    Plot_busy_time() function.
    Function to plot the number of seconds per day of potential
    communication time between a ground station and satellite.

--- python-files/analysis.py
+++ python-files/analysis.py
@@ -12,20 +12,32 @@ def plot_busy_time(daily_totals, client=None, sat=None):
     If sat not specified, means one client all sats
     If neither, use generic "client" and "sat"
     """
-    # Find total access time for tree
-    #     Including: one gs tree, one sat tree, one gs/one sat tree
-    accesstime_total = 0
-    for item in passtree:
-        accesstime_total = accesstime_total + item.data.duration
+    import seaborn
+    import matplotlib.pyplot as plt
+    spd = 86400  # seconds/day
 
+    fig = plt.figure(1)
+    if client is None and sat is not None:
+        fig.suptitle('%s Access Time for All Ground Stations' % (sat))
+    elif sat is None and client is not None:
+        fig.suptitle('All Satellite Access Time for %s Ground Station' % (client))
+    elif sat is None and client is None:
+        fig.suptitle('All Satellite Access time for All Ground Stations')
+    else:
+        fig.suptitle('%s Access time for %s Ground Station' % (sat, client))
 
-    # Find access time per day for tree
+    s1 = plt.subplot()
+    s1.plot(daily_totals, 'b-')
+    s1.set_ylim(ymin=0, ymax=spd)
+    plt.xlabel('Days from Epoch')
+    plt.ylabel('Busy time (sec/day)')
 
-    # Including: one gs tree, one sat tree, one gs/one sat tree
-    start, end = passtree.begin(), passtree.end()
+    #s2 = plt.subplot(222)
+    #s2.plot(daily_totals, 'b-')
+    #s2.set_ylim(ymin=0, ymax=ymax)
+    #plt.xlabel('Days from Epoch')
 
-    # get day of first pass, start at midnight
-    q = start.replace(hour=0, minute=0, second=0, microsecond=0)
+    plt.show()
 
     # get day of last pass, end at midnight
     c = end.replace(hour=0, minute=0, second=0, microsecond=0)
