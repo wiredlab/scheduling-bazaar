bccd75dd68e38c36a8e397a3d4a6bbbc3e857c11
Grace Freigang <grace.freigang@valpo.edu>
2017-08-01 18:01:17 +0000

    Add pass2requests to schedulers.py
    pass2requests used in each scheduling method

--- python-files/schedulers.py
+++ python-files/schedulers.py
@@ -29,3 +29,33 @@ def random_scheduler(passes, clients, debug=False):
 
 
 # def max_sat_el(passes, clients, debug=False):
+
+
+def pass2request(pd):
+    """Take a pass (as returned from db.getpasses() and construct a request
+    dict for the Network to send to a Client.
+
+    The bounty is SNC (SatNOGS Credits) with an amount set to the pass duration
+    in seconds.
+
+    When transmitted over a network, this is then converted to JSON.
+    """
+    d = pd.data
+    job = {'id': random.randrange(2**16),  # fake an ID number
+           'start': d.start.isoformat(' '),
+           'end': d.end.isoformat(' '),
+           'ground_station': d.gs,
+           'tle0': satellites[d.sat]['tle0'],
+           'tle1': satellites[d.sat]['tle1'],
+           'tle2': satellites[d.sat]['tle2'],
+           'frequency': -1,
+           'mode': 'null',
+           'transmitter': 'asdfasdasdfadsf',
+           }
+    duration = (d.end - d.start).total_seconds()
+    bounty = [{'currency': 'SNC', 'amount': duration}]
+
+    request = {'job': job,
+               'bounty': bounty,
+               }
+    return request
