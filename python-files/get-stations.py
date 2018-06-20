798dd3e86e9cd9e3b0917bec313db391d0c5b153
Dan White <dan@whiteaudio.com>
2018-06-20 09:08:56 -0500

    script to fetch the list of registered GS on Network

---
+++ python-files/get-stations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Utility to get the station information from a SatNOGS Network server.
+
+Collects the paginated objects into a single JSON list and stores in a file.
+"""
+
+import json
+import requests
+
+
+NETWORK = 'https://network.satnogs.org/api/stations'
+STATIONS_JSON = 'network-stations.json'
+
+
+stations = []
+r = requests.get(NETWORK)
+
+stations.extend(r.json())
+nextpage = r.links.get('next')
+
+while nextpage:
+    r = requests.get(url=nextpage['url'])
+    stations.extend(r.json())
+    nextpage = r.links.get('next')
+
+
+with open(STATIONS_JSON, 'w') as fp:
+    json.dump(stations, fp)
+
