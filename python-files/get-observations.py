cd4e7c5bc10c8e946ddf31d99a249a5a97b2dfda
Dan White <dan@whiteaudio.com>
2018-07-04 15:32:00 -0500

    Update get-observations.py
    Store data as a dict with observation ID as keys.
    
    json.dump() coerces all keys to strings, load back in as integers.
    
    Walks the pages returned from the API and continues until no entries are new.

--- python-files/get-observations.py
+++ python-files/get-observations.py
@@ -18,21 +18,46 @@ def get(url):
     print(url)
     return requests.get(url)
 
-observations = []
+
+try:
+    with open(OBSERVATIONS_JSON) as f:
+        data = json.load(f)
+        # json.dump() coerces to string keys
+        # convert keys back to integers
+        observations = {}
+        for k,v in data.items():
+            print(k)
+            observations[int(k)] = v
+        # observations = {v['id']:v for k,v in data.items()}
+except IOError:
+    observations = {}
+
+
+
+def update(o, observations):
+    o_id = o['id']
+    print(o_id)
+    if o_id not in observations:
+        observations[o_id] = o
+        was_new = True
+    else:
+        observations.update(o)
+        was_new = False
+    return was_new
+
+
 r = get(OBSERVATIONS_API)
-# r = requests.get(OBSERVATIONS_API)
+updated = [update(o, observations) for o in r.json()]
+any_updated = any(updated)
 
-observations.extend(r.json())
 nextpage = r.links.get('next')
-
-while nextpage:
-    # r = requests.get(nextpage['url'])
+while any_updated and nextpage:
     r = get(nextpage['url'])
-    observations.extend(r.json())
-    nextpage = r.links.get('next')
-
-observations = sorted(observations, key=lambda s: s['id'])
+    updated = [update(o, observations) for o in r.json()]
+    print(updated)
+    any_updated = any(updated)
+    if any_updated:
+        nextpage = r.links.get('next')
 
 with open(OBSERVATIONS_JSON, 'w') as fp:
     json.dump(observations, fp, sort_keys=True, indent=2)
-
