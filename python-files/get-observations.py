96d3a6bdf03a2a0edc484ff74071a7d493fff993
Dan White <dan@whiteaudio.com>
2018-07-05 16:04:55 -0500

    get-observations.py: handle bad data and only update new data

--- python-files/get-observations.py
+++ python-files/get-observations.py
@@ -3,16 +3,21 @@
 """
 Utility to get observations from a SatNOGS Network server.
 
-Collects the paginated objects into a single JSON list and stores in a file.
+Collects the paginated objects into a single JSON mapping keyed by observation id
+and stores in a file.
 """
 
 import json
 import requests
 
+# uncomment this when testing the script and not getting new data
+# import requests_cache
+# requests_cache.install_cache(expire_after=60*60*3)
 
 OBSERVATIONS_API = 'https://network.satnogs.org/api/observations'
 OBSERVATIONS_JSON = 'observations.json'
 
+MAX_EXTRA_PAGES = 10
 
 def get(url):
     print(url)
@@ -26,16 +31,13 @@ try:
         # convert keys back to integers
         observations = {}
         for k,v in data.items():
-            print(k)
             observations[int(k)] = v
-        # observations = {v['id']:v for k,v in data.items()}
 except IOError:
     observations = {}
 
 
-
 def update(o, observations):
-    o_id = o['id']
+    o_id = int(o['id'])
     print(o_id)
     if o_id not in observations:
         observations[o_id] = o
@@ -51,13 +53,27 @@ updated = [update(o, observations) for o in r.json()]
 any_updated = any(updated)
 
 nextpage = r.links.get('next')
-while any_updated and nextpage:
+extra_pages = MAX_EXTRA_PAGES
+
+while (extra_pages > 0) and nextpage:
     r = get(nextpage['url'])
     updated = [update(o, observations) for o in r.json()]
     print(updated)
-    any_updated = any(updated)
-    if any_updated:
-        nextpage = r.links.get('next')
+
+    nextpage = r.links.get('next')
+
+    # heuristic to capture recent updates to observations
+    # keep fetching pages of observations until we see
+    # MAX_EXTRA_PAGES in a row with no updates
+    if not any(updated):
+        extra_pages -= 1
+    else:
+        extra_pages = MAX_EXTRA_PAGES
+    print(extra_pages)
+
+
+# filter out the bad data
+obs = {k:v for k,v in observations.items() if isinstance(v, dict)}
 
 with open(OBSERVATIONS_JSON, 'w') as fp:
-    json.dump(observations, fp, sort_keys=True, indent=2)
+    json.dump(obs, fp, sort_keys=True, indent=2)
