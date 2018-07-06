14426c01e2161a0dae315d96deb52eee83faa367
Dan White <dan@whiteaudio.com>
2018-07-06 17:21:58 -0500

    use AMSAT TLE if not available from CelesTrak

--- python-files/get-satellites.py
+++ python-files/get-satellites.py
@@ -12,10 +12,12 @@ import sys
 from datetime import datetime
 import json
 import sqlite3
+from itertools import islice
 
 import requests
 import orbit
 
+DO_PRINT = False
 
 # default expire time is 24 hours
 orbit.tle.requests_cache.uninstall_cache()
@@ -27,8 +29,24 @@ URL = 'https://db.satnogs.org/api/satellites'
 SATELLITES_JSON = 'satellites.json'
 TLE_DB = 'tle.sqlite'
 
+AMSAT = 'https://www.amsat.org/tle/current/nasabare.txt'
 
-# fetch known satellites
+# go ahead and fetch the AMSAT TLEs once for this session
+amsat = {}
+r = requests.get(AMSAT)
+
+lines = iter(r.text.splitlines())
+while lines:
+    triple = islice(lines, 3)
+    tle = [x.rstrip() for x in triple]
+    if len(tle) == 3:
+        norad = int(tle[1][2:7])
+        amsat[norad] = tle
+    else:
+        break
+
+
+# fetch satellites known to the network
 r = requests.get(URL)
 satellites = r.json()
 
@@ -56,12 +74,19 @@ for sat in satellites:
     try:
         # fetch information from CelesTrak
         body = orbit.satellite(norad)
+        if DO_PRINT: print('{} TLE from CelesTrak'.format(norad))
+
+        line0, line1, line2 = body.tle_raw
 
         # TODO: merge information from
         # https://www.amsat.org/tle/current/nasabare.txt
     except IndexError:
         # bad parse of data, typically response was "No TLE found"
-        continue
+        if norad in amsat:
+            line0, line1, line2 = amsat[norad]
+            if DO_PRINT: print('{} TLE from AMSAT'.format(norad))
+        else:
+            if DO_PRINT: print('{} no TLE'.format(norad))
     else:
         epoch = body.tle_parsed._epoch.datetime()
 
@@ -70,9 +95,9 @@ for sat in satellites:
           'INSERT INTO tle VALUES (?,?,?,?,?,?);',
           (norad,
            epoch,
-           body.tle_raw[0],
-           body.tle_raw[1],
-           body.tle_raw[2],
+           line0,
+           line1,
+           line2,
            datetime.utcnow().isoformat()))
         # 'INSERT OR IGNORE INTO ...' will suppress the exception
     except sqlite3.IntegrityError:
