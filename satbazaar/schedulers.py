b897f5891a08a5268965c2cdf72bae92fea103c3
Dan White <dan@whiteaudio.com>
2018-07-12 22:36:58 -0500

    scheduler classes

--- satbazaar/schedulers.py
+++ satbazaar/schedulers.py
@@ -3,55 +3,80 @@
 import random
 
 
-def random_scheduler(passes, clients, satellites, debug=False):
-    """Randomly schedules passes on the given clients (which are already
-    instantiated with the desired client type).
 
-    passes - candidate passes for scheduling.
-    clients - dict of ground stations to use.
-    debug - Boolean.  Print out status markers for each.
-    """
-    passes = random.sample(passes, len(passes))
-    for pd in passes:
-        # create a request
-        r = pass2request(pd, satellites)
-        offer = clients[pd.data.gs].request(r)
-        if debug is True:
+class Scheduler:
+    """Base class for schedulers.  Inherit and implement __call__."""
+    def __init__(self, clients, satellites, passes=None, debug=False):
+        """Store dicts of clients and satellites for use later.
+        Do the scheduling if passes is given.
+        """
+        self.clients = clients
+        self.satellites = satellites
+        self.passes = passes
+        self.debug = debug
+        if passes is not None:
+            self(passes)
+
+    def __call__(self, passes):
+        raise NotImplementedError
+
+    def do_request(self, pd):
+        """Helper to take a PassTuple and make a request to the relevant client."""
+        r = pass2request(pd, self.satellites)
+        offer = self.clients[pd.data.gs].request(r)
+        if self.debug:
             if offer['status'] == 'accept':
                 print('*', end='', flush=True)
             else:
                 print('.', end='', flush=True)
-    if debug is True:
-        print()
-    return clients
+        return offer
 
 
-def first_start_scheduler(passes, clients, satellites, debug=False):
-    """Makes requests to stations in order of start time.
-
-    passes - candidate passes for scheduling.
-    clients - dict of ground stations to use.
-    debug - Boolean.  Print out status markers for each pass.
-
-    Returns updated clients with filled calendars.
-    """
-    # TODO: what is the sort key?
-    for pd in sorted(passes):
-        r = pass2request(pd, satellites)
-        offer = clients[pd.data.gs].request(r)
-        if debug is True:
-            if offer['status'] == 'accept':
-                print('*', end='', flush=True)
-            else:
-                print('.', end='', flush=True)
-    if debug is True:
-        print()
-    return clients
-
-# def max_gs_el(passes, clients, debug=False):
+class RandomScheduler(Scheduler):
+    """Make requests in random order."""
+    def __call__(self, passes):
+        passes = random.sample(passes, len(passes))
+        for pd in passes:
+            self.do_request(pd)
+        return self.clients
 
 
-# def max_sat_el(passes, clients, debug=False):
+class FirstScheduler(Scheduler):
+    """Make requests in order of start time."""
+    def __call__(self, passes):
+        for pd in sorted(passes, key=lambda p: p.begin):
+            self.do_request(pd)
+
+
+class LastScheduler(Scheduler):
+    """Make requests by end time, starting with the last pass."""
+    def __call__(self, passes):
+        for pd in sorted(passes, key=lambda p: p.end, reverse=True):
+            self.do_request(pd)
+
+
+class DurationScheduler(Scheduler):
+    """Order requests by duration of passes."""
+    def __call__(self, passes):
+        for pd in sorted(passes, key=lambda p: (p.end - p.begin), reverse=True):
+            self.do_request(pd)
+
+
+class EndStartScheduler(Scheduler):
+    """Should be same result as FirstScheduler."""
+    def __call__(self, passes):
+        now = passes.begin()
+        last = passes.end()
+        while now < last:
+            tree = passes.search(now, last, strict=True)
+            s = sorted(tree, key=lambda i: i.begin)
+            if len(s) == 0:
+                break
+            pd = s[0]
+            self.do_request(pd)
+            now = pd.end
+
+
 
 
 def pass2request(pd, satellites):
