0e66e4eed5da5d4507813968e7bb835f3adad2ba
Dan White <dan@whiteaudio.com>
2020-12-15 13:28:34 -0600

    hack

--- python-files/get-observations.py
+++ python-files/get-observations.py
@@ -10,6 +10,7 @@ and stores in a file.
 import gzip
 import json
 import requests
+import sys
 
 import pprint
 print = pprint.pprint
@@ -19,13 +20,19 @@ print = pprint.pprint
 # requests_cache.install_cache(expire_after=60*60)
 
 OBSERVATIONS_API = 'https://network.satnogs.org/api/observations'
-OBSERVATIONS_JSON = 'observations.json.gz'
+#OBSERVATIONS_JSON = 'observations.json.gz'
+OBSERVATIONS_JSON = 'observations.json'
 # OBSERVATIONS_API = 'https://network-dev.satnogs.org/api/observations'
 # OBSERVATIONS_JSON = 'observations-dev.json.gz'
 
 # MAX_EXTRA_PAGES = 50
-MAX_EXTRA_PAGES = 5
+# MAX_EXTRA_PAGES = 5
 MAX_EXTRA_PAGES = 20
+
+# RETRY_UNKNOWN_VETTED_OBS = True
+RETRY_UNKNOWN_VETTED_OBS = False
+
+
 
 client = requests.session()
 def get(url):
@@ -94,24 +101,31 @@ any_updated = any(updated)
 nextpage = r.links.get('next')
 extra_pages = MAX_EXTRA_PAGES
 
-while (extra_pages > 0) and nextpage:
-    r = get(nextpage['url'])
-    # network-dev returns a 500 server error at some point 350+ pages in
-    try:
+try:  # allow KeyboardInterrupt to stop the update but save data
+    while (extra_pages > 0) and nextpage:
+        r = get(nextpage['url'])
+        # network-dev returns a 500 server error at some point 350+ pages in
+        # try:
         updated = [update(o, observations) for o in r.json()]
-    except:
-        print(r)
+        # except Exception as e:
+            # print('Error on response:', r)
+            # print(e)
+            # raise e
 
-    nextpage = r.links.get('next')
+        nextpage = r.links.get('next')
 
-    # heuristic to capture recent updates to observations
-    # keep fetching pages of observations until we see
-    # MAX_EXTRA_PAGES in a row with no updates
-    if not any(updated):
-        extra_pages -= 1
-    else:
-        extra_pages = MAX_EXTRA_PAGES
-    print(extra_pages)
+        # heuristic to capture recent updates to observations
+        # keep fetching pages of observations until we see
+        # MAX_EXTRA_PAGES in a row with no updates
+        if not any(updated):
+            extra_pages -= 1
+        else:
+            extra_pages = MAX_EXTRA_PAGES
+        print(extra_pages)
+
+except Exception as e: #anything...    KeyboardInterrupt:
+    print(e)
+    print('Stopping...')
 
 
 # filter out the bad data
@@ -124,18 +138,13 @@ for k in badkeys:
 
 
 
-print('***********************************')
-print('* NOT UPDATING unknown vetted obs *')
-# print('* updating unknown vetted obs *')
-print('***********************************')
-# if True:
-if False:
+print('')
+if RETRY_UNKNOWN_VETTED_OBS:
+    print('******************************')
+    print('* Getting unknown vetted obs')
+    print('******************************')
     try:  # allow KeyboardInterrupt to stop the update but save data
         # try to fetch old obs with no vetting
-        print('')
-        print('******************************')
-        print('* Getting unknown vetted obs')
-        print('******************************')
         for o_id, o in sorted(observations.items(), reverse=True):
             if o['vetted_status'] == 'unknown':
                 r = get(OBSERVATIONS_API + '/' + str(o_id))
@@ -146,12 +155,22 @@ if False:
                 else:
                     update(d, observations)
     except: #anything...    KeyboardInterrupt:
+        e = sys.exc_info()
+        print(e)
         print('Stopping...')
+else:
+    print('***********************************')
+    print('* NOT UPDATING unknown vetted obs *')
+    print('***********************************')
 
-
+print('Finished getting new/updated obs.')
+print('Saving ' + OBSERVATIONS_JSON)
 if OBSERVATIONS_JSON.endswith('.gz'):
     with gzip.open(OBSERVATIONS_JSON, 'wt') as fp:
         json.dump(observations, fp, sort_keys=True, indent=2)
 else:
     with open(OBSERVATIONS_JSON, 'w') as fp:
         json.dump(observations, fp, sort_keys=True, indent=2)
+
+print('Done!')
+
