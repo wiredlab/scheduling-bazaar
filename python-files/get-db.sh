29c7dc8906cc294090c7aa90d7a8ab9431cc7305
Dan White <dan@whiteaudio.com>
2020-12-15 13:27:53 -0600

    get-db.sh:  request JSON content explicitly

--- python-files/get-db.sh
+++ python-files/get-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for endpoint in "modes" "satellites" "transmitters"; do
-    wget -O - "https://db.satnogs.org/api/$endpoint/" \
+    wget -O - "https://db.satnogs.org/api/$endpoint/?format=json" \
         | jq '.' > "db-$endpoint.json"
 done
 
